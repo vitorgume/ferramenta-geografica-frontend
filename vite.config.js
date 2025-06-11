b92125abb949158a004a0b5fb62b5d5aed9f23f1
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-06-11 15:20:05 -0300

    Ajustando plug do Setry

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "gume-inteligencia-comercial",
-    project: "javascript-react",
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    include: "./dist",
-    ignore: ["node_modules", "vite.config.js"],
-  })],
-
-  build: {
-    sourcemap: true
-  }
+  plugins: [react()]
 })
